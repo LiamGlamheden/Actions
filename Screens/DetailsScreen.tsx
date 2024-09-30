563c289d75db10c9a30c37ba6ac50e7809630a16
Liam <04liagla@skola.boras.se>
2024-09-30 11:46:43 +0200

    added naviagtion

---
+++ Screens/DetailsScreen.tsx
@@ -0,0 +1,14 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { Text, View } from "react-native";
+import { RootStackParamList } from "../Navigators/RootStackNavigator";
+
+type Props = NativeStackScreenProps<RootStackParamList, "Details">;
+
+
+export default function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Detai</Text>
+    </View>
+  );
+}
