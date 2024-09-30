6d6e50a927a4a8facf829f01d102f114d51dd3f2
Liam <04liagla@skola.boras.se>
2024-09-30 14:21:14 +0200

    Lint test

--- Screens/DetailsScreen.tsx
+++ Screens/DetailsScreen.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function DetailsScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Detail</Text>
+      <Text>Details</Text>
     </View>
   );
 }
