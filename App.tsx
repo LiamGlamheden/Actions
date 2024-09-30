dad6e60d6b068fe252793a17cda0d9bad237a19e
Liam <04liagla@skola.boras.se>
2024-09-30 14:20:29 +0200

    Added lint

--- App.tsx
+++ App.tsx
@@ -1,9 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet } from 'react-native';
 import RootStackNavigator from './Navigators/RootStackNavigator';
 
-const Stack = createNativeStackNavigator();
 
 function App() {
   return (
@@ -14,12 +11,3 @@ function App() {
 }
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- Screens/DetailsScreen.tsx
+++ Screens/DetailsScreen.tsx
@@ -1,8 +1,5 @@
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { Text, View } from 'react-native';
-import { RootStackParamList } from '../Navigators/RootStackNavigator';
 
-type Props = NativeStackScreenProps<RootStackParamList, 'Details'>;
 
 export default function DetailsScreen() {
   return (
