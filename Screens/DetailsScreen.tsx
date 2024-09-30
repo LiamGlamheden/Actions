f3f8363bbc5f3a85414c80db48fceaadd06d531d
Liam <04liagla@skola.boras.se>
2024-09-30 13:31:20 +0200

    Fixes

--- Screens/DetailsScreen.tsx
+++ Screens/DetailsScreen.tsx
@@ -1,9 +1,8 @@
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Text, View } from "react-native";
-import { RootStackParamList } from "../Navigators/RootStackNavigator";
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Text, View } from 'react-native';
+import { RootStackParamList } from '../Navigators/RootStackNavigator';
 
-type Props = NativeStackScreenProps<RootStackParamList, "Details">;
-
+type Props = NativeStackScreenProps<RootStackParamList, 'Details'>;
 
 export default function DetailsScreen() {
   return (
--- Screens/HomeScreen.tsx
+++ Screens/HomeScreen.tsx
@@ -1,16 +1,17 @@
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Button, Text, View } from "react-native";
-import { RootStackParamList } from "../Navigators/RootStackNavigator";
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Button, Text, View } from 'react-native';
+import { RootStackParamList } from '../Navigators/RootStackNavigator';
 
-type Props = NativeStackScreenProps<RootStackParamList, "Home">;
+type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
-export default function HomeScreen({ navigation }: Props) 
-{
-    return(
-        <View>
-            <Text>Home Screen</Text>
-            <Button title= "Details" onPress={() => navigation.navigate("Details")}></Button>
-        </View>
-    )
+export default function HomeScreen({ navigation }: Props) {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      <Button
+        title="Details"
+        onPress={() => navigation.navigate('Details')}
+      ></Button>
+    </View>
+  );
 }
-
